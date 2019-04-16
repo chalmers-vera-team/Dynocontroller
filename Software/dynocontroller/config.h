509bfdd4a7afd6cf27a6c140e211d76dc1b3f737
Oskar Johansson <oskaj@student.chalmers.se>
2019-04-16 17:48:37 +0200

    Updated to correct input pins
    Resolves #2

--- Software/dynocontroller/config.h
+++ Software/dynocontroller/config.h
@@ -2,10 +2,10 @@
 #define CONFIG_H
 
 // All pins needed 
-#define PIN_BRAKECONTROL D3
+#define PIN_BRAKECONTROL 3
 #define PIN_CONTROLAUTOINPUT A1
 #define PIN_CONTROLMANUALINPUT A3 
-#define PIN_CONTROLMODEINPUT D8
+#define PIN_CONTROLMODEINPUT 8
 
 
 #define INVERTMANUALINPUT 1 // 1 - If manual input is inverted
