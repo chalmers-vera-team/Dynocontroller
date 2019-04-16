e2e3920c3a6a210f3e481f7df9b36f2ccac3134f
Oskar Johansson <oskaj@student.chalmers.se>
2019-04-16 09:29:26 +0200

    Updated pins, not verified

--- Software/dynocontroller/config.h
+++ Software/dynocontroller/config.h
@@ -2,13 +2,13 @@
 #define CONFIG_H
 
 // All pins needed 
-#define PIN_BRAKECONTROL 10
-#define PIN_CONTROLAUTOINPUT 10 
-#define PIN_CONTROLMANUALINPUT 10 
-#define PIN_CONTROLMODEINPUT 10
+#define PIN_BRAKECONTROL D3
+#define PIN_CONTROLAUTOINPUT A1
+#define PIN_CONTROLMANUALINPUT A3 
+#define PIN_CONTROLMODEINPUT D8
 
 
-#define INVERTMANUALINPUT 0 // 1 - If manual input is inverted
+#define INVERTMANUALINPUT 1 // 1 - If manual input is inverted
 
 
 #endif
